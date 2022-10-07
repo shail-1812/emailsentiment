35470209052f97fcae7d7e71dcf8903871e64fd8
shail-1812 <88042019+shail-1812@users.noreply.github.com>
2022-10-07 18:48:41 +0530

    version 2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='emailsentiment',
-    version='0.0.1',
+    version='0.0.2',
     author='Shail Patel',
     author_email='shailpatel1812@gmail.com',
     description='Sentiment analysis on emails',
