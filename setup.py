fdb1bc2deabaa78e2a38599c53a6af15cea09c14
Shail Patel <shailpatel1812@gmail.com>
2022-10-11 22:41:15 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='emailsentiment',
+    version='0.0.3',
+    author='Shail Patel',
+    author_email='shailpatel1812@gmail.com',
+    description='Sentiment analysis on emails',
+    long_description_content_type="text/markdown",
+    url='https://github.com/shail-1812/emailsentiment.git',
+    packages=['emailsentiment'],
+    install_requires=['keras_preprocessing', 'spacy', 'nlppreprocess'],
+    include_package_data=True,
+    package_data={'': ['emailsentiment/*.sav', 'emailsentiment/*.pickle']},
+)
